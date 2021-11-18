f9ce652c7b93965be2cb299bb66cc53059f9b09b
Jayice <1185430411@qq.com>
2021-11-18 21:03:59 +0800

    arr

---
+++ ch3/src/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    let x:(i32,u32,u8)=(12,321,21);
+    let y=x.0;
+    let z=x.1;
+    let k=x.2;
+
+
+    let arr=[1,2,3,4];
+    let arr1=[2;6];
+    let arr2:[i32;2]=[1,2];
+
+    println!("{}",arr[0]);
+}
